d780a785bf9961e3779121e6c5e47f32b20d268e
sayanpal469 <sayanpal469@gmail.com>
2026-02-26 19:51:39 +0530

    t

--- src/app.js
+++ src/app.js
@@ -8,8 +8,8 @@ app.use(cors());
 
 
 // Normal body parsers (used for all other routes)
-app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 app.use(express.static("public"));
 
 // Other routes
